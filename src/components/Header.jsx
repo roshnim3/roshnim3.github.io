275ce6342dc14167a79eda417b31d74d94d49fa3
Roshni Mathew <roshnimathew@Roshnis-MacBook-Pro-4.local>
2024-12-26 02:47:27 -0500

    switched from browser routing to hash routing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import './Header.css'; 
+import { HashLink as Link } from 'react-router-hash-link';
+import './Header.css';
 
 function Header() {
     return (
       <header className="header">
         <nav className="navbar">
-          <a href="#home" className="logo">Roshni Mathew</a>
+          <Link to="/#home" className="logo">Roshni Mathew</Link>
           <ul className="nav-links">
-            <li><a href="#home" className="sections">Home</a></li>
-            <li><a href="#projects" className="sections">Projects</a></li>
-            <li><a href="#experience" className="sections">Experience</a></li>
-            <li><a href="#research" className="sections">Research</a></li>
+            <li><Link to="/#home" className="sections">Home</Link></li>
+            <li><Link to="/#projects" className="sections">Projects</Link></li>
+            <li><Link to="/#experience" className="sections">Experience</Link></li>
+            <li><Link to="/#research" className="sections">Research</Link></li>
             <li><a href="/assets/roshnimathew-resume.pdf" className="resume-btn">Resume</a></li>
           </ul>
         </nav>
